81e076f7581914545e543d4211463480cda9de14
mdroberts74 <mikedroberts88@gmail.com>
2018-08-27 11:29:46 -0500

    My first create.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Company
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company newCompany = new Company("My Name", new DateTime(2018, 12, 1));
+
+            Employee mike = new Employee()
+            {
+                firstName = "Mike",
+                lastName = "Roberts",
+                title = "Software Developer",
+                startDate = new DateTime(2018, 5, 21)
+
+            };
+            Employee adelaide = new Employee()
+            {
+                firstName = "Adelaide",
+                lastName = "Yoder",
+                title = "Software Developer",
+                startDate = new DateTime(2018, 5, 21)
+
+            };
+            Employee aaron = new Employee()
+            {
+                firstName = "Aaron",
+                lastName = "Miller",
+                title = "Software Developer",
+                startDate = new DateTime(2018, 5, 21)
+
+            };
+            string returnStatement = newCompany.Hire(mike);
+            System.Console.WriteLine(returnStatement);
+            newCompany.Hire(adelaide);
+            newCompany.Hire(aaron);
+
+
+
+            foreach (Employee person in newCompany.currentEmployees)
+            {
+                System.Console.WriteLine($" We hired {person.firstName}");
+            }
+        }
+    }
+    // Create three employees
+
+
+    // Assign the employees to the company
+
+    /*
+        Iterate the company's employee list and generate the
+        simple report shown above
+    */
+}
---
+++ employee.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Company
+{
+
+    public class Employee
+    {
+
+        // Some readonly properties (let's talk about gets, baby)
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string title { get; set; }
+        public DateTime startDate { get; set; }
+
+        // Create a public property for holding a list of current employees
+
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+
+            The constructor will set the value of the public properties
+        */
+    }
+
+}
